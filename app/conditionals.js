59f33bbc8bcb663aa37a50e04f903a094bfdad8f
Bud <rollandcampbell@msn.com>
2019-08-13 17:01:09 -0600

    javascript answers

--- app/conditionals.js
+++ app/conditionals.js
@@ -2,7 +2,11 @@
 
 function largerNum(num1, num2) {
     // you code here
-
+    if (num1 > num2) {
+        return num1;
+    } else if (num2 > num1) {
+        return num2;
+    }
 }
 
 // 2. Given two numbers, amount correct and amount possible of a test, return the corresponding letter grade.
@@ -14,7 +18,21 @@ function largerNum(num1, num2) {
 // output: "F"
 
 function testGrader(score, possible) {
-
+    if (score / possible >= .90) {
+        return "A";
+    }
+    else if (score / possible >= .80 && score / possible <= .89) {
+        return "B";
+    }
+    else if (score / possible >= .70 && score / possible <= .79) {
+        return "C";
+    }
+    else if (score / possible >= .60 && score / possible <= .69) {
+        return "D"
+    }
+    else if (score / possible <= .59) {
+        return "F";
+    }
 }
 
 
@@ -30,12 +48,34 @@ function testGrader(score, possible) {
 
 function timeOfDayGreeting(hour) {
     // you code below
-
+    if (hour >= 5 && hour <= 11) {
+        return "good morning";
+    }
+    if (hour >= 12 && hour <= 17) {
+        return "good afternoon";
+    }
+    if (hour >= 18 && hour <= 21) {
+        return "good evening";
+    }
+    if (hour >= 22 && hour <= 24) {
+        return "good night";
+    }
+    if (hour >= 1 && hour <= 4) {
+        return "good night";
+    }
 }
 
 //4. Write a function that will take in a number and return 'fever' if it indicates a fever (over 98.6) and additionally if the person should go to the hospital (at or above 103) 'fever go to hospital' (hint: try this with string concatenation), if it is under return 'no fever'
 function isFever(temp) {
-
+    if (temp <= 98.6) {
+        return "no fever";
+    }
+    if (temp > 98.6 && temp < 103) {
+        return "fever";
+    }
+    if (temp >= 103) {
+        return "fever go to hospital"
+    }
 }
 
 //5. Write a function that takes in a car object, if it is not moving then return true
@@ -47,7 +87,12 @@ let myCar = {
 }
 
 function isStopped(car) {
-
+    if (car.moving == false) {
+        return true;
+    }
+    if (car.moving == true) {
+        return false;
+    }
 }
 
 //6. Write a function that returns true if a dish is yours and is dirty, or false if one of the statements is false
@@ -58,5 +103,10 @@ let dish = {
 }
 
 function washDish(dish) {
-
+    if (dish.yourDish == true && dish.isDirty == true) {
+        return true;
+    }
+    if (dish.yourDish == false || dish.isDirty == false) {
+        return false;
+    }
 }
--- app/strings.js
+++ app/strings.js
@@ -1,12 +1,25 @@
 // Write a function that returns the character from a string at a given position
 function letterAtPosition(str, position) {
-
+  //finds the position in the string
+  let i = str.charAt(position);
+  //shows on console
+  console.log(i)
+  //sends i to memory to be used
+  return i;
 }
 
 // Write a function that will join two strings with an optional seperating character 
 function concat(str1, str2, seperator = " ") {
+  //combines the two strings, places seperator between
+  let i = str1.concat(seperator, str2)
+  console.log(i)
+  return i;
 }
 
 // Write a function that will capitalize the letter at a given position and return it
 function capitalizer(str, position) {
+  //finds the position in the string, then capitalizes it
+  return str.charAt(position).toUpperCase()
+  //capitalizes
+  console.log(capitalizer);
 }
